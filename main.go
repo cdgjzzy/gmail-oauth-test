b2da34bf4aab5033b7b2bc00b45f6eb9968d605a
zzy <chaoyangzhu@ddmarketinghub.com>
2022-08-10 18:07:12 +0800

    init

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	r := gin.Default()
+
+	// 注册handlers
+	r.GET("/redirect", redirectHandler) // 跳转到同意屏幕
+	r.GET("/code", code)                // 接收授权服务器的code回调
+	r.GET("/token", token)              // 接收授权服务器的token回调
+
+	// 在5000端口启动
+	r.Run(":5000")
+}
---
+++ requests.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func getToken(code string) (err error) {
+
+	log.Println("get token request")
+
+	bodyFormat := "code=%s&client_id=%s&client_secret=%s&redirect_uri=%s&grant_type=authorization_code"
+	var body = strings.NewReader(fmt.Sprintf(bodyFormat, code, client_id, client_secret, token_redirect_uri))
+	response, err := http.Post(tokenUri, "application/x-www-form-urlencoded", body)
+	if err != nil {
+		return err
+	}
+	if response.StatusCode != http.StatusOK {
+		return fmt.Errorf("response error code:%v", response.StatusCode)
+	}
+
+	return nil
+}
+
+func refreshToken(refresh_token string) error {
+
+	log.Println("refresh token request")
+
+	bodyFormat := "client_id=%s&client_secret=%s&refresh_token=%s&grant_type=refresh_token"
+	var body = strings.NewReader(fmt.Sprintf(bodyFormat, client_id, client_secret, refresh_token))
+	response, err := http.Post(tokenUri, "application/x-www-form-urlencoded", body)
+	if err != nil {
+		return err
+	}
+	if response.StatusCode != http.StatusOK {
+		return fmt.Errorf("response error code:%v", response.StatusCode)
+	}
+
+	return nil
+}
